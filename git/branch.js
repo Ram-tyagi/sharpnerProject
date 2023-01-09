728de9b24cad9f066ea98c9db722b6477a2fd2c8
Ram-tyagi <ramtyagi04@gmail.com>
2023-01-09 13:58:19 +0530

    addingli

--- git/branch.js
+++ git/branch.js
@@ -1,6 +1,6 @@
 var items = document.getElementByClassName('list-group-item');
 console.log(items);
-
+items[2].style.backgroundcolor='green';
 for(var i=0;i<items.length;i++){
     items[i].style.fontWieght = 'bold';
 }
